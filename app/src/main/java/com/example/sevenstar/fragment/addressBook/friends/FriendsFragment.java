f94e82cfcf9b6d2b388860f88930b6ed1bc10dce
ad <3162562449@qq.com>
2018-03-01 20:33:21 +0800

    小调整

--- app/src/main/java/com/example/sevenstar/fragment/addressBook/friends/FriendsFragment.java
+++ app/src/main/java/com/example/sevenstar/fragment/addressBook/friends/FriendsFragment.java
@@ -1,6 +1,7 @@
 package com.example.sevenstar.fragment.addressBook.friends;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -114,6 +115,11 @@ public class FriendsFragment extends BaseFragment implements FriendsView {
     public void findFailed(Throwable e) {
         $Log(e.getMessage());
         $toast("网络出错！");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
     }
 
     private void setEaseList(FriendsBean friendsBean) {
